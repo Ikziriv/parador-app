3bded1f0eaad2c104ac1c74e72414a006d471b44
Rizky Ivan Darmawan <ivanriski007@gmail.com>
2022-03-15 13:51:31 +0700

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,8 +5,8 @@
 
 <div class="w-full h-screen bg-gray-50 overflow-hidden relative">
     <div class="grid grid-cols-12 flex justify-center items-center w-full h-screen">
-        <div class="col-span-full md:col-span-4 w-full h-auto md:px-8 md:py-4">
-            <div class="flex flex-col w-full h-auto md:py-4 border-b mb-8">
+        <div class="col-span-full md:col-span-4 w-full h-auto px-4 md:px-8 py-4 md:py-4">
+            <div class="flex flex-col w-full h-auto py-4 md:py-4 border-b mb-8">
                 <button 
                 on:click={() => {
                     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -37,13 +37,14 @@
                 </div>
             </div>
         </div>
-        <div class="col-span-full md:col-span-8 w-full h-screen relative bg-gradient-to-r from-cyan-500 to-blue-500">
-            <div class="absolute inset-y-0 right-0 w-12 h-auto flex justify-center items-center md:mr-8">
+        <div class="col-span-full md:col-span-8 w-full h-screen relative">
+            <div class="absolute inset-y-0 right-0 w-20 md:w-12 h-screen flex justify-center items-center md:mr-8">
                 <div class="w-12 h-12 bg-gray-800 rounded-full flex justify-center items-center border">
                     <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
                 </div>
             </div>
-            <img alt="HomeImage" src="https://images.unsplash.com/photo-1562790351-d273a961e0e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" class="object-cover object-center h-screen w-full hidden md:block"/>
+            <img alt="HomeImage" src="https://images.unsplash.com/photo-1562790351-d273a961e0e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" class="object-cover object-center h-auto md:h-screen w-full"/>
+            <div class="absolute inset-x-0 bottom-0 w-full h-16 bg-gray-900 opacity-75 block md:hidden"></div>
         </div>
     </div>
 </div>
